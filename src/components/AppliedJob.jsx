583fd9e4971a463344b02a09b91824ea02775cce
shojol021 <shojol021@gmail.com>
2023-04-12 02:06:04 +0600

    blog section has updated with question answer

--- src/components/AppliedJob.jsx
+++ src/components/AppliedJob.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import { CurrencyDollarIcon, MapPinIcon } from '@heroicons/react/24/solid';
+import { useNavigate } from 'react-router-dom';
 
-const AppliedJob = ({appliedJob}) => {
-    const { logoUrl, jobType, companyName, jobMode, workingHours, location, salaryRange, id } =  appliedJob;
+const AppliedJob = ({ appliedJob }) => {
+    const { logoUrl, jobType, companyName, jobMode, workingHours, location, salaryRange, id } = appliedJob;
+    const navigate = useNavigate();
+    const handleViewDetails = () => {
+        navigate(`../job-details/${id}`)
+    }
     return (
         <div className='m-3 border-solid border border-sky-300 hover:border-purple-400 rounded-lg bg-gradient-to-br from-sky-200  to-purple-200 p-3'>
-            <img className='w-24 mb-3 rounded-md' src={logoUrl} alt="" />
-            <h3 className='text-2xl font-bold'>{jobType}</h3>
-            <p className='text-gray-500 mb-3'>{companyName}</p>
-            <p className='text-sky-700 inline border-solid border-2 border-sky-300 rounded px-1 bg-sky-300 me-2'>{jobMode}</p>
-            <p className='text-sky-700 inline border-solid border-2 border-sky-300 rounded px-1 bg-sky-300'>{workingHours}</p>
-            <div className='flex my-3'>
-                <MapPinIcon className="h-6 w-6 text-gray-400 me-3" />
-                <p className='me-5 text-gray-500'>{location}</p>
-                <CurrencyDollarIcon className="h-6 w-6 text-gray-400 me-3" />
-                <p className='text-gray-500'>Salary: {salaryRange}</p>
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center'>
+                    <div className=' w-32 h-32 border border-solid border-sky-300 rounded-md flex justify-center items-center bg-white'>
+                        <img className='w-24 mb-3 rounded-md' src={logoUrl} alt="" />
+                    </div>
+                    <div className='ms-9'>
+                        <h3 className='text-2xl font-bold'>{jobType}</h3>
+                        <p className='text-gray-500 mb-3'>{companyName}</p>
+                        <p className='text-sky-700 inline border-solid border-2 border-sky-300 rounded px-1 bg-sky-300 me-2'>{jobMode}</p>
+                        <p className='text-sky-700 inline border-solid border-2 border-sky-300 rounded px-1 bg-sky-300'>{workingHours}</p>
+                        <div className='flex my-3'>
+                            <MapPinIcon className="h-6 w-6 text-gray-400 me-3" />
+                            <p className='me-5 text-gray-500'>{location}</p>
+                            <CurrencyDollarIcon className="h-6 w-6 text-gray-400 me-3" />
+                            <p className='text-gray-500'>Salary: {salaryRange}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='me-5'>
+                    <button onClick={handleViewDetails} className='btn btn-secondary'>View Details</button>
+                </div>
             </div>
-            <button onClick={[]} className='btn btn-secondary'>View Details</button>
         </div>
     );
 };
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, Bar, CartesianGrid, ComposedChart, Legend, Line, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const [marks, setMarks] = useState([]);
+    useEffect(() => {
+        fetch('marks.json')
+            .then(res => res.json())
+            .then(marks => setMarks(marks))
+    }, [])
     return (
-        <div>
-            Statistics
+        <div className='m-9'>
+            <h2 className='text-center font-bold text-3xl mt-5 mb-3'>Assignment Analytics</h2>
+            <h3 className='text-center mb-2'>Avg Assignment Marks: 60</h3>
+            <div className=''>
+                <ComposedChart className='mx-auto' width={1000} height={400} data={marks}>
+                    <YAxis></YAxis>
+                    <XAxis dataKey="name"></XAxis>
+                    <Tooltip></Tooltip>
+                    <Legend></Legend>
+                    <CartesianGrid stroke="#f5f5f5" />
+                    <Area type="monotone" dataKey="average" fill="#8884d8" stroke="#8884d8" />
+                    <Bar dataKey="marks" barSize={20} fill="#413ea0" />
+
+                </ComposedChart>
+            </div>
         </div>
     );
 };
