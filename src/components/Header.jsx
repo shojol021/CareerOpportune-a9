b94150d3c0f170c0022f0db2181fee6176fb0daa
shojol021 <shojol021@gmail.com>
2023-04-12 11:12:48 +0600

    made navbar responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,52 @@ import { Link, NavLink, useLocation } from 'react-router-dom';
 const Header = () => {
 
     const location = useLocation();
-    
+
     const currentLocation = location.pathname;
     const isJobDetails = currentLocation.slice(0, 12) == '/job-details';
     const isJobs = currentLocation == '/jobs';
     const isStatistics = currentLocation == '/statistics';
     const isBlogs = currentLocation == '/blogs';
-    const headerStyle = isJobDetails || isJobs || isStatistics || isBlogs? "bg-[url('/banner1.png')]" : "";
+    const headerStyle = isJobDetails || isJobs || isStatistics || isBlogs ? "bg-[url('/banner1.png')]" : "";
     return (
-        <nav className={`text-center flex justify-between md:p-9 ${headerStyle}`}>
-            <Link to='/'><h1 className='text-2xl font-bold'>CareerOpportune</h1></Link>
-            <ul className='flex justify-center'>
-                <li className='me-5'><NavLink  to='/' className={({ isActive }) => (isActive ? 'active' : 'default')}>Home</NavLink></li>
-                <li className='me-5'><NavLink to='/statistics' className={({ isActive }) => (isActive ? 'active' : 'default')}>Statistics</NavLink></li>
-                <li className='me-5'><NavLink to='jobs' className={({ isActive }) => (isActive ? 'active' : 'default')}>Applied Jobs</NavLink></li>
-                <li className='me-5'><NavLink to='blogs' className={({ isActive }) => (isActive ? 'active' : 'default')}>Blog</NavLink></li>
-            </ul>
-            <button className='btn btn-primary'>Start Applying</button>
-        </nav>
+        // <nav className={`text-center flex justify-between md:p-9 ${headerStyle}`}>
+        //     <Link to='/'><h1 className='text-2xl font-bold'>CareerOpportune</h1></Link>
+        //     <ul className='flex justify-center'>
+        //         <li className='me-5'><NavLink  to='/' className={({ isActive }) => (isActive ? 'active' : 'default')}>Home</NavLink></li>
+        //         <li className='me-5'><NavLink to='/statistics' className={({ isActive }) => (isActive ? 'active' : 'default')}>Statistics</NavLink></li>
+        //         <li className='me-5'><NavLink to='jobs' className={({ isActive }) => (isActive ? 'active' : 'default')}>Applied Jobs</NavLink></li>
+        //         <li className='me-5'><NavLink to='blogs' className={({ isActive }) => (isActive ? 'active' : 'default')}>Blog</NavLink></li>
+        //     </ul>
+        //     <button className='btn btn-primary'>Start Applying</button>
+        // </nav>
+
+        <div className={`navbar bg-base-100 text-center flex justify-between md:p-9 ${headerStyle}`}>
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <li className='me-5'><NavLink to='/' className={({ isActive }) => (isActive ? 'active' : 'default')}>Home</NavLink></li>
+                    <li className='me-5'><NavLink to='/statistics' className={({ isActive }) => (isActive ? 'active' : 'default')}>Statistics</NavLink></li>
+                    <li className='me-5'><NavLink to='jobs' className={({ isActive }) => (isActive ? 'active' : 'default')}>Applied Jobs</NavLink></li>
+                    <li className='me-5'><NavLink to='blogs' className={({ isActive }) => (isActive ? 'active' : 'default')}>Blog</NavLink></li>
+                    </ul>
+                </div>
+                <Link to='/'><h1 className='text-2xl font-bold'>CareerOpportune</h1></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li className='me-5'><NavLink to='/' className={({ isActive }) => (isActive ? 'active' : 'default')}>Home</NavLink></li>
+                    <li className='me-5'><NavLink to='/statistics' className={({ isActive }) => (isActive ? 'active' : 'default')}>Statistics</NavLink></li>
+                    <li className='me-5'><NavLink to='jobs' className={({ isActive }) => (isActive ? 'active' : 'default')}>Applied Jobs</NavLink></li>
+                    <li className='me-5'><NavLink to='blogs' className={({ isActive }) => (isActive ? 'active' : 'default')}>Blog</NavLink></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Get started</a>
+            </div>
+        </div>
     );
 };
 
